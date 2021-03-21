b0fd7ddee76ee2404848e6707292b404d828ea8f
Ibrahim Ali <47118191+ibrahiimmohd@users.noreply.github.com>
2021-03-21 14:33:47 -0400

    Added product functionality

---
+++ app/models/product.server.model.js
@@ -0,0 +1,21 @@
+// Load the Mongoose module and Schema object
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+// Define a new 'ProductSchema'
+var ProductSchema = new Schema({
+    productId: { type: String, unique: true, required: true },
+    title: String,
+    content: String,
+    price: Number,
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+    updated: {
+        type: Date,
+        default: Date.now,
+    },
+});
+// Create the 'Product' model out of the 'ProductSchema'
+mongoose.model('Product', ProductSchema);

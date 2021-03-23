09fa16d2f0f3a45a7f32af84aa6d5c20cb3780db
Giri Siva <giri_siva51@outlook.com>
2021-03-23 00:13:55 -0400

    Created staff model

---
+++ app/models/staff.server.model.js
@@ -0,0 +1,69 @@
+// Load the module dependencies
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+//Define a schema
+const Schema = mongoose.Schema;
+
+// Define a new 'StaffSchema'
+// Basic Model, add more field(s) as needed
+const StaffSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    role: String,
+    email: {
+        type: String,
+        // Validate the email format
+        match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    },
+    password: {
+        type: String,
+        // Validate the 'password' value length
+        validate: [
+            (password) => password && password.length > 6,
+            'Password should be longer',
+        ],
+    },
+    created: {
+        type: Date,
+        // Create a default 'created' value
+        default: Date.now,
+    },
+    updated: {
+        type: Date,
+        // Create a default 'updated' value
+        default: Date.now,
+    },
+});
+
+// Set the 'fullname' virtual property
+StaffSchema.virtual('fullName')
+    .get(function () {
+        return this.firstName + ' ' + this.lastName;
+    })
+    .set(function (fullName) {
+        const splitName = fullName.split(' ');
+        this.firstName = splitName[0] || '';
+        this.lastName = splitName[1] || '';
+    });
+
+// Use a pre-save middleware to hash the password
+// before saving it into database
+StaffSchema.pre('save', function (next) {
+    this.password = bcrypt.hashSync(this.password, saltRounds);
+    next();
+});
+
+// Create an instance method for authenticating Staff
+StaffSchema.methods.authenticate = function (password) {
+    return this.password === bcrypt.hashSync(password, saltRounds);
+};
+
+// Configure the 'StaffSchema' to use getters and virtuals when transforming to JSON
+StaffSchema.set('toJSON', {
+    getters: true,
+    virtuals: true,
+});
+
+// Create the 'Staff' model out of the 'StaffSchema'
+mongoose.model('Staff', StaffSchema);

ead3650516030fc9a78854a9cd85c4fb6253502a
Ibrahim Ali <47118191+ibrahiimmohd@users.noreply.github.com>
2021-03-22 23:18:33 -0400

    Added menu list

--- app/routes/app.server.routes.js
+++ app/routes/app.server.routes.js
@@ -3,6 +3,8 @@ const HomeController = require('../controllers/home.server.controller');
 const LoginController = require('../controllers/login.server.controller');
 const ProfileController = require('../controllers/profile.server.controller');
 const ProductController = require('../controllers/product.server.controller');
+const MenuListController = require('../controllers/menu_list.server.controller');
+
 // Define the routes module' method
 module.exports = function (app) {
 
@@ -20,24 +22,17 @@ module.exports = function (app) {
 
     //app.post('/profile/save', LoginController.verifyUser);
 
-    //show the 'add_product' page if a GET request is made to /product
+    //Products
     app.route('/add_product').get(ProductController.renderAdd);
-
-    // a post request to /product will execute createProduct method in product.server.controller
     app.route('/products').post(ProductController.createProduct);
-    
     app.route('/list_products').get(ProductController.readProduct);
-
-    // Set up the 'courses' parameterized routes
     app.route('/list_products/:productId')
         .get(ProductController.read)
         .put(ProductController.updateByProductId)
         .delete(ProductController.deleteByProductId);
 
-    // Set up the 'productId' parameter middleware
-    //All param callbacks will be called before any handler of 
-    //any route in which the param occurs, and they will each 
-    //be called only once in a request - response cycle, 
-    //even if the parameter is matched in multiple routes
     app.param('productId', ProductController.findProductByProductId);
+
+    //Menu List
+    app.route('/menu_list').get(MenuListController.readMenuList);
 };
